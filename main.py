4b476e02ed8773f068bab51626f7aae2a0fce7d3
William Wrolstad <wrolstad.william@gmail.com>
2026-06-15 23:26:48 -0700

    fixed particle generation

--- main.py
+++ main.py
@@ -38,19 +38,20 @@ class ParticleSource:
     def spawn(self,xs,dt):
         self.t+=dt
         while self.t*self.rate>1 and xs.shape[0]<self.MAX_PARTICLES:
-            xs = np.append(xs,self.x)
+            xs = np.vstack([xs,self.x])
             self.t-=1/self.rate
         return xs
     
 class ParticleSystem:
     def __init__(self,x0) -> None:
-        self.xs = np.empty(shape=[0,2])
+        self.xs = np.empty(shape=[0,2])        
         self.vs = np.empty(shape=self.xs.shape)
         self.sources = []
 
     def update_pos(self,dt):
         self.random_vel()
-        self.xs = self.xs+self.vs*dt
+        if self.xs.size>0:
+            self.xs = self.xs+self.vs*dt
 
     def add_source(self,x,rate,max_particles=1000):
         self.sources.append(ParticleSource(x,rate,max_particles))
@@ -60,7 +61,7 @@ class ParticleSystem:
             self.xs = source.spawn(self.xs,dt)
 
     def random_vel(self):
-        self.vs = np.interp(np.random.rand(*self.vs.shape),[0,1],[-1,1])
+        self.vs = np.interp(np.random.rand(*self.xs.shape),[0,1],[-1,1])
 
     def step(self,dt):
         self.update_sources(dt)
@@ -82,66 +83,6 @@ class ParticleRenderer:
         for i in range(xs.shape[0]):
             pygame.draw.circle(self.screen,"white",xs[i],2)
 
-
-
-
-
-# def run_sim():
-#     pygame.init()
-#     pygame.font.init()
-#     screen = pygame.display.set_mode((1280, 720))
-#     clock = pygame.time.Clock()
-#     game_font = pygame.font.SysFont("arial",30)
-
-#     running = True
-    
-#     x0 = np.array([screen.get_width()/2,screen.get_height()/2])
-
-#     #grid = [np.array([x,y],np.float64) + x0 for x in range(-50,50,10) for y in range(-50,50,10)]
-
-#     #particles = [Particle(pos) for pos in grid]
-
-#     source  = ParticleSource(x0,max_particles=5000)
-#     particles = []
-    
-
-#     while running:
-
-#         #Events
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-        
-#         #Physics
-#         dt = clock.tick()
-#         source.spawn(particles,dt)
-#         for particle in particles:
-#             particle.update_pos(dt)
-
-#         #Rendering
-#         cmap = plt.get_cmap('magma')
-#         screen.fill("purple")
-#         MAX_RENDERED = 1000
-#         rendered_particles = 0
-#         for particle in particles:
-#             if particle.alive and rendered_particles<MAX_RENDERED:
-#                 rendered_particles+=1
-#                 pygame.draw.circle(screen,"white",particle.get_pos().tolist(),2)
-#             elif not particle.alive:
-#                 pygame.draw.circle(screen,"red",particle.get_pos().tolist(),2)
-#                 particles.remove(particle)
-#         txt_numpart = game_font.render(f'Number of Particles: {len(particles)}', True, "white")
-#         txt_renpart = game_font.render(f'Number of Rendered Particles: {rendered_particles}', True, "white")
-#         txt_frmrt = game_font.render(f'Framerate: {1/(dt/1000):.0f}FPS',True,"white")
-#         screen.blit(txt_numpart,(25,50))
-#         screen.blit(txt_renpart,(25,100))
-#         screen.blit(txt_frmrt, (25,150))
-#         pygame.display.update()
-#         pygame.display.flip()
-
-    
-#     pygame.quit()
-#     print("Goodbye")
 
 def run_sim():
     pygame.init()
@@ -172,6 +113,7 @@ def run_sim():
         #Physics
         dt = clock.tick()
         ps.step(dt)
+
 
         screen.fill("purple")
 
